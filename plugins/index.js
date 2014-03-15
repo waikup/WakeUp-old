49f6ab0d51224c745859f67629154eaded01124a
alexperezpaya <alejandro@perezpaya.net>
2014-03-15 04:09:01 +0100

    Some files created

---
+++ plugins/index.js
@@ -0,0 +1,17 @@
+//http://api.soundcloud.com/tracks/13158665/stream?client_id=5a8edbed865ed2b31acf4d9720696e7f
+
+var Speaker = require('speaker');
+
+var speaker = new Speaker({
+	channels: 2,          // 2 channels
+	bitDepth: 16,         // 16-bit samples
+	sampleRate: 44100     // 44,100 Hz sample rate
+});
+
+
+module.exports = function (callback){
+
+	//http://api.soundcloud.com/tracks/13158665/stream?client_id=5a8edbed865ed2b31acf4d9720696e7f
+
+
+};
